fe4a6c8d68dce6c2ffe0413ea2f8e2a003e5124b
Henry Cooksley <henry.cooksley@gmail.com>
2018-08-11 20:35:50 +0100

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_08_11_145547) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "comment"
